ff32d992d9db35d82ced74540cccda740baec720
kaito <iso.kaito0725@gmail.com>
2026-04-11 16:02:28 +0900

    メニュー画面（日付機能）

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,8 +17,39 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MenuController {
 
+	private static final String date = "date";
+
 	@GetMapping("/menu")
-	public String menu() {
+	public String menu(@RequestParam(name = date, required = false) String date, Model model) {
+
+		LocalDate baseDate;
+
+		// パラメータの日付を使用、なければ今日
+		if (date != null && !date.isEmpty()) {
+			baseDate = LocalDate.parse(date);
+		} else {
+			baseDate = LocalDate.now();
+		}
+
+		// yyyy年MM月dd日(E)の形式で今日の日付を取得
+		var formatterToday = DateTimeFormatter.ofPattern("yyyy年MM月dd日(E)", Locale.JAPANESE);
+		model.addAttribute("today", LocalDate.now().format(formatterToday));
+
+		// 日付計算
+		model.addAttribute("prevWeek", baseDate.minusWeeks(1).toString()); // 前週
+		model.addAttribute("prevDay", baseDate.minusDays(1).toString()); // 前日
+		model.addAttribute("nextDay", baseDate.plusDays(1).toString()); // 翌日
+		model.addAttribute("nextWeek", baseDate.plusWeeks(1).toString()); // 翌週
+
+		List<LocalDate> days = new ArrayList<>();
+
+		// スケジュール表示用の7日間
+		for (int i = 0; i < 7; i++) {
+			days.add(baseDate.plusDays(i));
+		}
+		model.addAttribute("days", days);
+
 		return "menu";
 	}
+
 }
